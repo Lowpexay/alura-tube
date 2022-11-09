7f3b39e8e84958d22f527ac8cd04a7c89d3415a0
Gabriel Gramacho <108759445+Lowpexay@users.noreply.github.com>
2022-11-09 19:57:09 -0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,125 @@
+import React from "react";
+import config from "../config.json";
+import styled from "styled-components";
+import {CSSReset} from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+function HomePage() {
+    // const mensagem = Designed by Low :D
+    const estilosHomePage = {
+        //backgroundColor: "Red" 
+    };
+
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+    
+
+    //console.log(config.playlists);
+
+    return (
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro}></Menu>
+                <Header></Header>
+                <TimeLine searchValue={valorDoFiltro} playlists={config.playlists}>
+                    Conteúdo
+                </TimeLine>
+            </div>
+        </>
+    );
+}
+
+export default HomePage
+
+//function Menu() {
+//    return (
+//        <div>
+//            Menu
+//        </div>
+//    )
+//}
+
+
+const StyledHeader = styled.div`
+        img {
+            width: 80px;
+            height: 80px;
+            border-radius: 50%;
+        }
+        .user-info {
+            display: flex;
+            align-items: center;
+            width: 100%;
+            padding: 16px 32px;
+            gap: 16px;
+        }
+    `;
+
+const StyledBanner = styled.div`
+    background-color: blue;
+    background-image: url(${config.bg});
+    height: 230px;
+
+`;
+
+function Header() {
+    return (
+        <StyledHeader>
+            <StyledBanner />
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`}></img>
+                <h2>
+                    {config.name}
+                </h2>
+                <p>
+                    {config.job}
+                </p>
+            </section>
+        </StyledHeader>
+    )
+}
+
+function TimeLine({searchValue, ...props}) {
+    //console.log("Dentro do componente", props.playlists);
+    const playlistsNames = Object.keys(props.playlists);
+    return (
+        <StyledTimeline>
+            {playlistsNames.map(function (playlistsNames) {
+                const videos = props.playlists[playlistsNames];
+                //console.log(videos);
+                //console.log(playlistsNames);
+                return (
+                    <section key={playlistsNames}>
+                        <h2>{playlistsNames}</h2>
+                        <div>
+                            
+                            {videos.filter((videos) => {
+                                const titleNormalized = videos.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized);
+                            }).map((videos) => {
+                                        return (
+                                            <a key={videos.url} href={videos.url}>
+                                                <img src={videos.thumb}></img>
+                                                <span>
+                                                    {videos.title}
+                                                </span>
+                                            </a>
+                                        )
+                                    })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
+
+
+
