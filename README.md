722614b8d0f3587809372eb8050e31a8aef10514
Gabriel Gramacho <108759445+Lowpexay@users.noreply.github.com>
2022-11-14 18:35:17 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-# Alura Tube
 
-Projeto da Imersão React
-
-| :computer: Vitrine.Dev |  Lowpexay   |
-| -------------  | --- |
-| :sparkles: Nome        | **Alura Tube**
-| :label: Tecnologias | React e javascript
-| :rocket: URL       | https://alura-tube-lowpexay.vercel.app/
-
-![](https://cdn.discordapp.com/attachments/690327547178909728/1041796108625641614/unknown.png)
-
-
+## Como está o projeto?
+- '_app.js': Carrega o setup base do projeto
+    - Aqui vão ficar os Providers de informação do nosso projeto
+    - CSSReset
---
+++ src/components/RegisterVideo/index.js
@@ -0,0 +1,57 @@
+import { StyledRegisterVideo } from "./styles";
+import React from "react";
+
+function useForm(propsDoForm) {
+    const[values, setValues] = React.useState(propsDoForm.initialValues);
+    return {
+        values,
+        handleChange: (evento) => {
+                const value = evento.target.value;
+                const name = evento.target.name
+                console.log(value);
+                setValues({
+                    ...values,
+                    [name]: value,
+                });
+        },
+        clearForm() {
+            setValues({});
+        }
+    };
+}
+
+export default function RegisterVideo() {
+    const formCadastro = useForm({
+        initialValues: {titulo: "", url: ""}
+    });
+    const [formVisivel, setFormVisivel] = React.useState(false);
+    return(
+        <StyledRegisterVideo>
+            <button className="add-video" onClick={() => setFormVisivel(true)}>
+                +
+            </button>
+            {formVisivel 
+                ? (
+                    <form onSubmit={(evento) =>{
+                        evento.preventDefault();
+                        setFormVisivel(false);
+                        formCadastro.clearForm();
+                    }}>
+                    <div>
+                    <button type="button" className="close-modal" onClick={() => setFormVisivel(false)}>
+                        X
+                    </button>
+                    <input placeholder="Titulo do vídeo" name="titulo" value={formCadastro.values.titulo} onChange={formCadastro.handleChange} />
+                    <input placeholder="URL" 
+                    name="url"
+                    value={formCadastro.values.url} onChange={formCadastro.handleChange} />
+                    <button type="submit">
+                        Cadastrar
+                    </button>
+                    </div>
+                </form>
+                )
+                : null}
+        </StyledRegisterVideo>
+    )
+}
